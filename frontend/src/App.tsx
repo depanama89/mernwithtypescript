f8a33f20ad8b7cfde2742a9b333039c70dbb44bc
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-04-18 12:05:40 +0200

    modification de package json ,suppresion des fichiers dans app.css,index css et creation du components note tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,25 +2,34 @@ import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import "./App.css";
 import { Note } from "./models/notes";
+import { parse } from "path";
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
 
   useEffect(() => {
     async function loadNotes() {
-      const response = await fetch("http://localhost:5000/api/notes", {
-        method: "GET",
-      });
+      try {
+        const response = await fetch("/api/notes", {
+          method: "GET",
+        });
+        const notes = await response.text();
+        const data = notes ? JSON.parse(notes) : null;
+        setNotes(data);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
     }
-  });
+    loadNotes();
+  }, []);
+
   const incrementer = () => {
     // setClickCount(clickCount + 1);
   };
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <Button onClick={incrementer}>clicked {clickCount} times</Button> */}
-      </header>
+      <header className="App-header">{JSON.stringify(notes)}</header>
     </div>
   );
 }
