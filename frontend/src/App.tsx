b720e9f27cc4249f0a4cfb409ca8ff3a520b3050
Patrick Kudiatu <kudiatutomanitu@gmail.com>
2025-04-18 10:08:21 +0200

    fetching data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import { Note } from "./models/notes";
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    async function loadNotes() {
+      const response = await fetch("http://localhost:5000/api/notes", {
+        method: "GET",
+      });
+    }
+  });
+  const incrementer = () => {
+    // setClickCount(clickCount + 1);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Button onClick={incrementer}>clicked {clickCount} times</Button> */}
       </header>
     </div>
   );
